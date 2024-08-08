9b9b45f7a9ed7a0e5562583c54f6c96b34169d93
beatrizodeiaescola <beatriz.cantao@escola.pr.gov.br>
2024-08-08 08:09:04 -0300

    bjh

---
+++ script.js
@@ -0,0 +1,20 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = querySelector("caixa-perguntas");
+const caixaAlternativas = document.querySelector("caixa-alternativas")
+const caixaResultados = document.querySelector(".caixa-resultado");
+const text0Resultado = document.querySelector("texto-resultado");
+
+const perguntas=[
+    {
+        enunciado: "No âmbito social, você prefere:",
+        alternativas: ["Alternativa 1", "Alternativa 2"]
+    }
+    {
+    enunciado: "No âmbito ambiental, você prefere:",
+    alternativas:["Alternativa 3", "Alternativa 4"]
+    },
+    {
+        enunciado: "No âmbito tecnológico, você prefere:",
+        alternativas: ["Alternativa 1", "Alternativa 2"]
+    }
+]
